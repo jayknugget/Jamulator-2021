0b022375f4251f29809d7b8d7d900a2b45d401e6
Ali Kedwaii <ali.kedwaii@utexas.edu>
2021-05-02 20:01:31 -0500

    Adding pause menu (unpolished)

---
+++ Assets/Scripts/PauseMenuUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuUI : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    void Awake()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if( Input.GetKeyDown( KeyCode.P ) )
+        {
+            if( PauseMenu.activeSelf )
+            {
+                OnClickResume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0.0f;
+        PauseMenu.SetActive(true);
+    }
+
+    public void OnClickMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene( "TitleScreen" );
+    }
+
+    public void OnClickResume()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
