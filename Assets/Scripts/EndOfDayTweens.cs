e60d5c17f3aeed6267b358f8ff3deb25fea8decc
Robert Guan <guan.robert@utexas.edu>
2021-05-03 01:20:47 -0500

    adding tweens to end of day

--- Assets/Scripts/EndOfDayTweens.cs
+++ Assets/Scripts/EndOfDayTweens.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class EndOfDayTweens : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField][Range(0,1)] private float paperBG_Y;
+    [SerializeField] private RectTransform paperBG;
+    [SerializeField][Range(0,1)] private float paperBGSquashFactor;
+    [SerializeField] private AnimationCurve paperBGScaleCurve;
+    [SerializeField][Range(0,2)] private float paperBGBounceUpTime;
+    [SerializeField][Range(0,2)] private float paperBGBounceUpWaitTime;
+    [SerializeField] private AnimationCurve paperBGBounceUpCurve;
+
+    [SerializeField] private Image pin;
+    [SerializeField][Range(0,2)] private float pinTime;
+    [SerializeField][Range(0,2)] private float pinScale;
+    [SerializeField] private AnimationCurve pinScaleCurve;
+    [SerializeField] private AnimationCurve pinPositionCurve;
+    [SerializeField] private AnimationCurve pinOpacityCurve;
+
     void Start()
     {
         
@@ -14,5 +30,14 @@ public class EndOfDayTweens : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private IEnumerator SadStamp()
+    {
+        Sequence pinSequence = DOTween.Sequence();
+        pinSequence.Append(pin.DOFade(0, pinTime).SetEase(pinOpacityCurve).From());
+        pinSequence.Join(pin.GetComponent<RectTransform>().DOScale(pinScale, pinTime).SetEase(pinScaleCurve).From());
+        // pinSequence.Append(mainCamera.DOShakePosition(.3f,3,20,100,true));
+        yield return pinSequence.WaitForCompletion();
     }
 }
